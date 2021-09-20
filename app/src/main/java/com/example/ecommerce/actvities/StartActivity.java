03b9512ed242f4cb65d72feac464d2ee23b56e39
kenthigh14 <kenthigh11@gmail.com>
2021-09-20 18:14:49 -0500

    Start Activity Update

--- app/src/main/java/com/example/ecommerce/actvities/StartActivity.java
+++ app/src/main/java/com/example/ecommerce/actvities/StartActivity.java
@@ -4,20 +4,38 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 import com.example.ecommerce.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import static android.content.ContentValues.TAG;
 
 public class StartActivity extends AppCompatActivity {
+
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+        auth = FirebaseAuth.getInstance();
 
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user != null) {
+            // User is signed in
+            Intent i = new Intent(StartActivity.this, MainActivity.class);
+            i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(i);
+        } else {
+            // User is signed out
+            Log.d(TAG, "onAuthStateChanged:signed_out");
+
+        }
 
     }
-
 
     public void joinnow(View view) {
 
@@ -30,4 +48,5 @@ public class StartActivity extends AppCompatActivity {
         Intent intent = new Intent(StartActivity.this, LoginActivity.class);
         startActivity(intent);
     }
+
 }
