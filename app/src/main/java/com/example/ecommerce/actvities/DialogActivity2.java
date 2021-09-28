4baf4f6535369394bc86459a67af6a2175d7ab4f
kenthigh14 <kenthigh11@gmail.com>
2021-09-28 09:34:11 -0500

    ewealth discount v3

--- app/src/main/java/com/example/ecommerce/actvities/DialogActivity2.java
+++ app/src/main/java/com/example/ecommerce/actvities/DialogActivity2.java
@@ -1,6 +1,7 @@
 package com.example.ecommerce.actvities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.net.Uri;
@@ -12,17 +13,32 @@ import com.example.ecommerce.R;
 
 public class DialogActivity2 extends AppCompatActivity {
 
-    Button cancel, ewalth;
+    Button verify, ewealth;
+
+    Toolbar toolbar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dialog2);
 
-        cancel = findViewById(R.id.btn_cancel);
-        ewalth = findViewById(R.id.btn_okay);
+        toolbar = findViewById(R.id.my_ewealthdiscount);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_baseline_arrow_back_24);
 
-        cancel.setOnClickListener(new View.OnClickListener() {
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        ewealth = findViewById(R.id.ewealth);
+       verify = findViewById(R.id.verify);
+
+        verify.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(DialogActivity2.this, MainActivity.class));
@@ -31,10 +47,10 @@ public class DialogActivity2 extends AppCompatActivity {
 
         });
 
-        ewalth.setOnClickListener(new View.OnClickListener() {
+        ewealth.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String url = "https://www.oldrichllc.com";
+                String url = "https://play.google.com/store/apps/details?id=com.ewealthapp";
 
                 Intent i = new Intent(Intent.ACTION_VIEW);
                 i.setData(Uri.parse(url));
